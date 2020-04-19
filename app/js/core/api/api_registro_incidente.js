6e57c2a82300f48a80a259ab3a25278b5d63ec01
alejandroVeraQuintero <veraa567@gmail.com>
2020-04-19 18:13:12 -0500

    Se realizo la crecion de consumo de api para back-end se realizo en registrat clientes y incidencia

--- app/js/core/api/api_registro_incidente.js
+++ app/js/core/api/api_registro_incidente.js
@@ -1,27 +1,42 @@
-const URL = '';
-const  BUTTON = document.querySelector('#registrarIncidencia');
-const data  = {
-    idIncidencia : parseInt ((Math.random () * 1000), 10) ,
-    fechaCapturaIncidente:document.getElementById ('fecha').value,
-    estadoIncidente: document.getElementById('estado').value,
-    definicionProblema : document.getElementById('definicionProblema').value,
-    identificacionProblema : document.getElementById ('identificacionProblema').value,
-    idUsuario : document.getElementById('identificacion').value,
-    tipoIncidente : document.getElementById('tipoIncidente').value
-        }
+const URL = 'http://localhost:3000/comments';
+const BUTTON = document.querySelector('#registrarIncidencia');
+let DATA
+const  createData = () => {
+     INCIDENT = {
+        idIncidencia  : parseInt((Math.random() * 1000), 10) ,
+        fechaCapturaIncidente : document.getElementById('date').value,
+        definicionProblema : document.getElementById('definitionProblem').value ,
+        identificacionProblema : document.getElementById('identificationProblem').value ,
+        finalizacionIncidente : "0",
+        diasIncidente  : "0",
+        estadoIncidente : document.getElementById('state').value ,
+        idUsuario : document.getElementById('id').value ,
+        tipoIncidente : document.getElementById('indenceType').value
+    }
+}
+
+
 const registrarIncidencia = () => {
-    
+
     fetch(URL, {
         method: 'POST',
-        body : JSON.stringify(data),
-        headers:{
+        body: JSON.stringify(INCIDENT),
+        headers: {
             'Content-Type': 'application/json'
-          }
+        },
+        mode: 'cors'
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then ((data) =>{
+        console.log(data);
     })
     
+
 }
 
 BUTTON.addEventListener('click', () => {
-    debugger;
-  post();
+    createData();
+    registrarIncidencia();
 })
